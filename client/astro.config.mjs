0ce773a863380549ac79d282fb1c43fb76a3e95a
ein <ein@jp9.org>
2023-03-26 19:59:06 +0200

    Configure client & server project structure

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -6,5 +6,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), react()]
+  integrations: [tailwind(), astroI18next(), react()],
+  server: { port: 5001 }
 });
