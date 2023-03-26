ee74d4ba7cb1876634e472bae75d958fbec2a370
ein <ein@jp9.org>
2023-03-27 00:25:37 +0200

    Delete connect between primary and foreign key

--- server/db/seeds/02_tasks.js
+++ server/db/seeds/02_tasks.js
@@ -8,19 +8,16 @@ exports.seed = function(knex, Promise) {
           title: 'Vaccuum the floors',
           description: 'Vaccum the living room and all bedroom',
           is_complete: false,
-          user_id: 2
         },
         {
           title: 'Clean the car',
           description: 'Wash, wax and vacuum the car',
           is_complete: false,
-          user_id: 1,
         },
         {
           title: 'Buy groceries',
           description: 'Milk, bread, cheese, eggs, flour',
           is_complete: true,
-          user_id: 3,
         }
       ]);
     });
