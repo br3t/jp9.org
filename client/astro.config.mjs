f53a198d1cc3f78862c81661fe02889d25e1aded
ein <ein@jp9.org>
2023-03-28 18:56:30 +0200

    Configure dev proxy middleware for backend

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -4,8 +4,13 @@ import astroI18next from "astro-i18next";
 
 import react from "@astrojs/react";
 
+import proxyMiddleware from './plugins/proxy-middleware.mjs'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), react()],
+  integrations: [tailwind(), astroI18next(), react(),  proxyMiddleware("/api", {
+    target: "http://localhost:3001",
+    changeOrigin: true,
+  }),],
   server: { port: 5001 }
 });
