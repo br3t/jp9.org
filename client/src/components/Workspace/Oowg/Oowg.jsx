c4cf73728b359b223e02ce0fe5791185fad2247d
ein <ein@jp9.org>
2023-04-05 00:07:15 +0200

    Little update UI for image uploader & add file names in demo images

--- client/src/components/Workspace/Oowg/Oowg.jsx
+++ client/src/components/Workspace/Oowg/Oowg.jsx
@@ -318,26 +318,50 @@ export default function React() {
                                                 key={index}
                                                 className="image-item"
                                               >
-                                                <img
-                                                  src={image["data_url"]}
-                                                  alt=""
-                                                  width="100"
-                                                />
+                                                {/*<img*/}
+                                                {/*  src={image["data_url"]}*/}
+                                                {/*  alt=""*/}
+                                                {/*  width="100"*/}
+                                                {/*/>*/}
+
                                                 <div className="image-item__btn-wrapper">
-                                                  <span
-                                                    onClick={() =>
-                                                      onImageUpdate(index)
-                                                    }
-                                                  >
-                                                    Update
-                                                  </span>
-                                                  <span
-                                                    onClick={() =>
-                                                      onImageRemove(index)
-                                                    }
-                                                  >
-                                                    Remove
-                                                  </span>
+                                                  <small>
+                                                    {image.file.name} [
+                                                    <svg
+                                                      onClick={() =>
+                                                        onImageUpdate(index)
+                                                      }
+                                                      className="hi-solid hi-refresh inline-block w-5 h-5"
+                                                      fill="currentColor"
+                                                      viewBox="0 0 20 20"
+                                                      xmlns="http://www.w3.org/2000/svg"
+                                                      aria-hidden="true"
+                                                    >
+                                                      <path
+                                                        fillRule="evenodd"
+                                                        d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z"
+                                                        clipRule="evenodd"
+                                                      />
+                                                    </svg>
+                                                    {" | "}
+                                                    <svg
+                                                      onClick={() =>
+                                                        onImageRemove(index)
+                                                      }
+                                                      className="hi-solid hi-document-remove inline-block w-5 h-5"
+                                                      fill="currentColor"
+                                                      viewBox="0 0 20 20"
+                                                      xmlns="http://www.w3.org/2000/svg"
+                                                      aria-hidden="true"
+                                                    >
+                                                      <path
+                                                        fillRule="evenodd"
+                                                        d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm1 8a1 1 0 100 2h6a1 1 0 100-2H7z"
+                                                        clipRule="evenodd"
+                                                      />
+                                                    </svg>
+                                                    ]
+                                                  </small>
                                                 </div>
                                               </div>
                                             ))}
