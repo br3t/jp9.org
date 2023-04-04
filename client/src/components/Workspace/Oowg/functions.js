3bcb8960bd156dbff1a1494a7d60000868b77d5e
ein <ein@jp9.org>
2023-04-04 17:38:23 +0200

    Refactor and change demo data

--- client/src/components/Workspace/Oowg/functions.js
+++ client/src/components/Workspace/Oowg/functions.js
@@ -7,8 +7,7 @@ const getDemoData = () => {
     title: "Lorem Ipsum - All the facts - Lipsum generator",
     description:
       "Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.",
-    htmlContent: `
-    <h1>Lorem Ipsum</h1>
+    htmlContent: `<h1>Lorem Ipsum</h1>
 <div>
 <div>
 <p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of <a href="#">Lorem Ipsum</a>.</p>
@@ -64,8 +63,7 @@ const getDemoData = () => {
 <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly <span style="text-decoration: underline;"><span style="color: #993300; text-decoration: underline;">simple</span></span> &nbsp;and easy to distinguish.</p>
 <p>In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.</p>
 </div>
-</div>
-    `,
+</div>`,
     demoImages: [
       {
         data_url:
